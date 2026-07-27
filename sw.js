a7a6c28492f4580c4b7fc5599fa60a26458b5e3a
Kaizo815 <Kaizo815@users.noreply.github.com>
2026-07-27 21:51:33 +0800

    Refine skeleton dragon Chinese names

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'dragon-story-shell-v9';
+const CACHE_NAME = 'dragon-story-shell-v10';
 const CORE_FILES = [
   './',
   './index.html',
   './style.css?v=20260726-fast6',
-  './data.js?v=20260727-polish3',
+  './data.js?v=20260727-skeleton4',
   './ticket-dragons.js?v=20260727-lang1',
   './app.js?v=20260727-lang1',
 ];
